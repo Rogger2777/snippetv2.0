1fedb1b697bb24277908aa7a7629c1bec96073d8
Rogger <750490794continental.edu.pe>
2023-11-15 23:31:18 -0500

    tests de parametros NO numericos que lanza exception terminado

--- src/tests/PruebaEcuacionSegundoGrado.py
+++ src/tests/PruebaEcuacionSegundoGrado.py
@@ -14,3 +14,19 @@ class PruebaEcuacionSegundoGrado(unittest.TestCase):
         with self.assertRaises(ValueError):
             ecuacionSegundoGrado.definirParametros("3.1", "5.2", "c")
 
+
+    def test_solucionESG_parametrosNoNumericos_lanzaException_subTest(self):
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+
+        items = (
+            {"Case": "Caso 01", "a": "a", "b":  "b", "c": "c"},
+            {"Case": "Caso 02", "a": "a", "b": 1, "c": 1},
+            {"Case": "Caso 03", "a": 1, "b": "aa", "c": 1},
+            {"Case": "Caso 03", "a": 1, "b": "3,1", "c": 1},
+        )
+
+        for item in items:
+            with self.subTest(item["Case"]):
+                self.assertTrue(True)
+                with self.assertRaises(ValueError):
+                    ecuacionSegundoGrado.definirParametros(item["a"], item["b"], item["c"])

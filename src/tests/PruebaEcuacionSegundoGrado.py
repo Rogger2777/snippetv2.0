a001516ed76828dda999df9b0da7e3f0cba93d32
Rogger <750490794continental.edu.pe>
2023-11-15 23:11:59 -0500

    tests raices reales y tests de excepciones terminadas

--- src/tests/PruebaEcuacionSegundoGrado.py
+++ src/tests/PruebaEcuacionSegundoGrado.py
@@ -5,6 +5,25 @@ from src.logica.EcuacionSegundoGrado import EcuacionSegundoGrado
 
 class PruebaEcuacionSegundoGrado(unittest.TestCase):
 
+    def test_solucionESG_parametrosNumericos_raicesReales(self):
+        # Arrange
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+        parametroA = 3
+        parametroB = -5
+        parametroC = 1
+
+        RaizEsperada1 = "1.43"
+        RaizEsperada2 = "0.23"
+
+        # Do
+        ecuacionSegundoGrado.definirParametros(parametroA, parametroB, parametroC)
+        RaizActual1, RaizActual2 = ecuacionSegundoGrado.solucionESG()
+
+        # Assert
+        self.assertEqual(RaizEsperada1, RaizActual1)
+        self.assertEqual(RaizEsperada2, RaizActual2)
+
+
     def test_solucionESG_parametrosNoNumericos_lanzaException(self):
         # Arrange
         ecuacionSegundoGrado = EcuacionSegundoGrado()
